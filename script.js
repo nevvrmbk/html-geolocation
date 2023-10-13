22817ce991effc174cccc313f5e15d91420babd8
nevvrmbk <99172207+nevvrmbk@users.noreply.github.com>
2023-10-13 21:43:05 +0300

    index.html, style.css, script.js added

---
+++ script.js
@@ -0,0 +1,19 @@
+window.addEventListener("DOMContentLoaded", async () => {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(position => {
+            document.getElementById("latitude").innerHTML = position.coords.latitude;
+            document.getElementById("longitude").innerHTML = position.coords.longitude;
+            document.getElementById("accuracy").innerHTML = position.coords.accuracy;
+            document.getElementById("altitude").innerHTML = position.coords.altitude;
+            document.getElementById("altitude-accuracy").innerHTML = position.coords.altitudeAccuracy;
+            document.getElementById("heading").innerHTML = position.coords.heading;
+            document.getElementById("speed").innerHTML = position.coords.speed;
+        }, error => {
+            document.writeln(error.message);
+        }, {
+            enableHighAccuracy: true,
+            maximumAge: 10000,
+            timeout: 10000,
+        });
+    }
+});
