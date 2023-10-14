41bb48e6997052fb75d592ec484cd9941d22c0e1
nevvrmbk <99172207+nevvrmbk@users.noreply.github.com>
2023-10-14 16:58:12 +0300

    script.js modified

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-window.addEventListener("DOMContentLoaded", async () => {
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(position => {
-            document.getElementById("latitude").innerHTML = position.coords.latitude;
-            document.getElementById("longitude").innerHTML = position.coords.longitude;
-            document.getElementById("accuracy").innerHTML = position.coords.accuracy;
-            document.getElementById("altitude").innerHTML = position.coords.altitude;
-            document.getElementById("altitude-accuracy").innerHTML = position.coords.altitudeAccuracy;
-            document.getElementById("heading").innerHTML = position.coords.heading;
-            document.getElementById("speed").innerHTML = position.coords.speed;
-        }, error => {
-            document.writeln(error.message);
-        }, {
-            enableHighAccuracy: true,
-            maximumAge: 1000000,
-            timeout: 1000000,
-        });
-    }
+// window.addEventListener("DOMContentLoaded", async () => {
+//     if ("geolocation" in navigator) {
+//         navigator.geolocation.getCurrentPosition(position => {
+//             document.getElementById("latitude").innerHTML = position.coords.latitude;
+//             document.getElementById("longitude").innerHTML = position.coords.longitude;
+//             document.getElementById("accuracy").innerHTML = position.coords.accuracy;
+//             document.getElementById("altitude").innerHTML = position.coords.altitude;
+//             document.getElementById("altitude-accuracy").innerHTML = position.coords.altitudeAccuracy;
+//             document.getElementById("heading").innerHTML = position.coords.heading;
+//             document.getElementById("speed").innerHTML = position.coords.speed;
+//         }, error => {
+//             document.writeln(error.message);
+//         }, {
+//             enableHighAccuracy: true,
+//             maximumAge: 1000000,
+//             timeout: 1000000,
+//         });
+//     }
+// });
+
+window.addEventListener("DOMContentLoaded", () => {
+    navigator.geolocation.watchPosition((position) => {
+        document.getElementById("latitude").innerHTML = position.coords.latitude;
+        document.getElementById("longitude").innerHTML = position.coords.longitude;
+        document.getElementById("accuracy").innerHTML = position.coords.accuracy;
+        document.getElementById("altitude").innerHTML = position.coords.altitude;
+        document.getElementById("altitude-accuracy").innerHTML = position.coords.altitudeAccuracy;
+        document.getElementById("heading").innerHTML = position.coords.heading;
+        document.getElementById("speed").innerHTML = position.coords.speed;
+    }, (error) => {
+        document.writeln(error.message);
+    }, {
+        enableHighAccuracy: true,
+        maximumAge: 1000000,
+        timeout: 1000000,
+    });
 });
